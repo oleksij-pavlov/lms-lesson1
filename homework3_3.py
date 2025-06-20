88b659b9f2e3b01c937222ffc26b7ab9bc1c0940
oleksij-pavlov <oleksij.pavlov@gmail.com>
2025-06-20 13:05:33 +0300

    comit 1 develop

---
+++ homework3_3.py
@@ -0,0 +1,12 @@
+lst = [9, "focus", [8,6,4], 98, 56]
+
+if len(lst) == 0:
+    result = [[], []]
+elif len(lst) % 2 == 0:
+    half = len(lst) // 2
+    result = [lst[:half], lst[half:]]
+else:
+    half = len(lst) // 2 + 1
+    result = [lst[:half], lst[half:]]
+
+print(result)

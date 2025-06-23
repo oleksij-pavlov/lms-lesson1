075ad07650504f2f6796b93349f0434eb25bd660
oleksij-pavlov <oleksij.pavlov@gmail.com>
2025-06-23 21:34:28 +0300

    comit 1 develop

--- homework4_1.py
+++ homework4_1.py
@@ -1,11 +1,11 @@
 lst = [1, 0, 13, 0, 0, 0, 5]
 result = []
-zero_count = 0
+i = 0
 for num in lst:
     if num != 0:
         result.append(num)
     else:
-        zero_count += 1
-for _ in range(zero_count):
+        i += 1
+for _ in range(i):
     result.append(0)
 print(result)
--- homework4_2.py
+++ homework4_2.py
@@ -1,9 +1,9 @@
 lst = [0, 1, 7, 2, 4, 8]
 if lst:
-    index = 0
-    for i in range(0, len(lst), 2):
-        index += lst[i]
-    result = index * lst[-1]
+    i = 0
+    for index in range(0, len(lst), 2):
+        i += lst[index]
+    result = i * lst[-1]
 else:
     result = 0
 print(result)

0c83dac12be522457454522b2e9712009bf26595
oleksij-pavlov <oleksij.pavlov@gmail.com>
2025-06-25 11:40:25 +0300

    comit 1 develop

--- homework4_3.py
+++ homework4_3.py
@@ -2,4 +2,4 @@ import random
 length = random.randint(3, 10)
 lst = [random.randint(0, 10) for _ in range(length)]
 lst1 = [lst[0], lst[2], lst[-2]]
-print(lst, lst1, sep='-->')
+print(lst, lst1, sep='->')

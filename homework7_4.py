03ff15e225f99580448d08cd2e935bc2811445f3
oleksij-pavlov <oleksij.pavlov@gmail.com>
2025-07-07 09:34:01 +0300

    comit 1 develop

--- homework7_4.py
+++ homework7_4.py
@@ -2,4 +2,4 @@ def common_elements():
     range_num_3 = {x for x in range(100) if x % 3 == 0}
     range_num_5 = {x for x in range(100) if x % 5 == 0}
     return range_num_3 & range_num_5
-assert common_elements() == {0, 75, 45, 15, 90, 60, 30, 1}
+assert common_elements() == {0, 75, 45, 15, 90, 60, 30}

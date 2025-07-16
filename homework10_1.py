a68756cfb4697bcba9387613449fb8aefff410c1
oleksij-pavlov <oleksij.pavlov@gmail.com>
2025-07-16 10:29:20 +0300

    comit 1 develop

---
+++ homework10_1.py
@@ -0,0 +1,16 @@
+def pow(x):
+    return x ** 2
+
+def some_gen(begin, end, func):
+
+    current = begin
+    for _ in range(end + 1):
+        yield begin
+        current = func(current)
+
+from inspect import isgenerator
+
+gen = some_gen(2, 4, pow)
+assert isgenerator(gen) == True, 'Test1'
+assert list(gen) == [2, 4, 16, 256], 'Test2'
+print('OK')

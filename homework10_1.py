3a13aa060ace3704d5f79ccd9180b3640ee18af0
oleksij-pavlov <oleksij.pavlov@gmail.com>
2025-07-16 10:15:58 +0300

    comit 1 develop

---
+++ homework10_1.py
@@ -0,0 +1,17 @@
+import string
+
+def first_word(text: str) -> str:
+    allowed = string.punctuation.replace("'", "")
+    for ch in allowed:
+        text = text.replace(ch, " ")
+
+    words = text.split()
+    return words[0]
+
+assert first_word("Hello world") == "Hello", 'Test1'
+assert first_word("greetings, friends") == "greetings", 'Test2'
+assert first_word("don't touch it") == "don't", 'Test3'
+assert first_word(".., and so on ...") == "and", 'Test4'
+assert first_word("hi") == "hi", 'Test5'
+assert first_word("Hello.World") == "Hello", 'Test6'
+print('OK')

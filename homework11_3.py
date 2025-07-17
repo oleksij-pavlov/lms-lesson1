5796b59a15c88d7b5b6091945403a6ff00a25053
oleksij-pavlov <oleksij.pavlov@gmail.com>
2025-07-17 20:40:27 +0300

    comit 1 develop

--- homework11_3.py
+++ homework11_3.py
@@ -1,7 +1,9 @@
 def is_even(number: int) -> bool:
     s = str(number)
-    if s == '0':
-        return False
     last_digit = s[-1]
-
     return last_digit in {'0', '2', '4', '6', '8'}
+
+assert is_even(2) == True, 'Test1'
+assert is_even(5) == False, 'Test2'
+assert is_even(0) == True, 'Test3'
+print('OK')

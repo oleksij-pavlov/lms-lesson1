fc73b681767b7599ba80a902b84b8ff175b7f5ce
oleksij-pavlov <olesij.pavlov@gmail.com>
2025-07-17 15:12:43 +0300

    comit 1 develop

--- homework3_1.py
+++ homework3_1.py
@@ -2,7 +2,7 @@ number_a = int(input("Введіть перше число: "))
 number_b = int(input("Введіть друге число: "))
 action = input("Введіть дію (+, -, *, /): ")
 
-
+#test
 
 if action == "/" and number_b == 0:
     print("Помилка: не можна ділити на нуль.")

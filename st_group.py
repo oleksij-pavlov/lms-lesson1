55d63a1efd341c49fc9577a50cfbe1b8d47c3e81
oleksij-pavlov <oleksij.pavlov@gmail.com>
2025-08-04 17:22:33 +0300

    comit 1 develop

---
+++ st_group.py
@@ -0,0 +1,25 @@
+from st_student import Student
+
+class Group:
+    def __init__(self, number):
+        self.number = number
+        self.group = set()
+
+    def add_student(self, student):
+        if isinstance(student, Student):
+            self.group.add(student)
+
+    def find_student(self, last_name):
+        for student in self.group:
+            if student.last_name == last_name:
+                return student
+        return None
+
+    def delete_student(self, last_name):
+        student = self.find_student(last_name)
+        if student:
+            self.group.remove(student)
+
+    def __str__(self):
+        all_students = '\n'.join(str(student) for student in self.group)
+        return f'Number: {self.number}\n{all_students}'
